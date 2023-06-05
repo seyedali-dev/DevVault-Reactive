9b97deda482e4c71fe54544ee4a52a415153cea3
HumbleLearner01 <learn.mike.helloworld@gmail.com>
2023-06-05 18:05:59 +0430

    Group-Management: create/search group for all Roles.

--- src/main/java/com/dev/vault/helper/payload/dto/SearchResponse.java
+++ src/main/java/com/dev/vault/helper/payload/dto/SearchResponse.java
@@ -1,9 +1,6 @@
 package com.dev.vault.helper.payload.dto;
 
-import com.dev.vault.model.group.ProjectMembers;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter
--- src/main/java/com/dev/vault/repository/group/ProjectRepository.java
+++ src/main/java/com/dev/vault/repository/group/ProjectRepository.java
@@ -1,7 +1,6 @@
 package com.dev.vault.repository.group;
 
 import com.dev.vault.model.group.Project;
-import com.dev.vault.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
