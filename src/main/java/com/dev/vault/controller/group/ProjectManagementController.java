c7b30fe1fc82860816dcb5672ccd4a960006b3c1
HumbleLearner01 <learn.mike.helloworld@gmail.com>
2023-06-19 08:48:19 +0430

    authorization error: role base is giving error

--- src/main/java/com/dev/vault/controller/group/ProjectManagementController.java
+++ src/main/java/com/dev/vault/controller/group/ProjectManagementController.java
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/proj_leader")
 @RequiredArgsConstructor
-@PreAuthorize("hasAnyRole('PROJECT_LEADER', 'TEAM_MEMBER', 'GROUP_ADMIN')")
+//@PreAuthorize("hasAnyRole('PROJECT_LEADER', 'TEAM_MEMBER', 'GROUP_ADMIN')")
 public class ProjectManagementController {
     private final ProjectManagementService projectManagementService;
 
-    @PreAuthorize("hasAnyAuthority('team_member:create_project','project_leader:create_project','group_admin:create_project')")
+    @PreAuthorize("hasRole('PROJECT_LEADER')")
     @PostMapping({"/create-project", "/create-group"})
     public ResponseEntity<?> createProjectOrGroup(@RequestBody ProjectDto projectDto) {
         return new ResponseEntity<>(projectManagementService.createProjectOrGroup(projectDto), HttpStatus.CREATED);
