1fcc7a0cb380e04382a595658da2614cbdf284f2
SayedAli <learn.mike.helloworld@gmail.com>
2023-06-27 23:04:10 +0430

    TaskManagement: 2 tasks getting created when creating a task.

--- src/main/java/com/dev/vault/helper/exception/JwtExpiredException.java
+++
@@ -1,9 +0,0 @@
-package com.dev.vault.helper.exception;
-
-import io.jsonwebtoken.ExpiredJwtException;
-
-public class JwtExpiredException extends RuntimeException {
-    public JwtExpiredException(String s, ExpiredJwtException e) {
-        super(s);
-    }
-}
